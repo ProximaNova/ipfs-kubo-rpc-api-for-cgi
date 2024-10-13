4a27bb68d5ab291ed75c8d8a71605d626f2c6fa6
ProximaNova <jaretrudd@gmail.com>
2024-10-13 04:56:40 -0600

    Update yt.sh

--- usr/lib/cgi-bin/yt.sh
+++ usr/lib/cgi-bin/yt.sh
@@ -3,17 +3,17 @@ echo "Content-type: text/html"
 echo
 echo "<pre>"
 v=$(echo -n $REQUEST_URI | sed "s/.*?v=//g" | sed "s/&.*//g")
-re=$(echo -n $REQUEST_URI | sed "s/.\{29\}//g")
+re=$(echo -n $REQUEST_URI | sed "s/.\{28\}//g") # related to var $v being one character long, also assumes 00.0.0.000/cgi-bin/yt.sh
 res=$(echo -n $re | sed "s/&..//g")
 #-- not H264 #yt-dlp "https://www.youtube.com/watch?v=$v" -f mp4 -f "bestvideo[height<=900]+bestaudio/best[height<=900]" -o /zc/put/cunt/$v.mp4
 #-- not H264 #yt-dlp "https://www.youtube.com/watch?v=$v" -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]" -o /zc/put/cunt/$v.mp4
 if [ -z $res ]; then
- echo NO RES
- echo "$res"
- yt-dlp "https://www.youtube.com/watch?v=$v" -f "bestvideo[height<=900]+bestaudio[ext=m4a]/best[height<=900]" -S "codec:h264" --restrict-filenames --windows-filenames -o "/zc/put/cunt/%(title)s-%(channel)s-%(upload_date)s-%(extractor)s-%(resolution)s-%(id)s.%(ext)s"
+    echo NO RES
+    echo "$res"
+    yt-dlp "https://www.youtube.com/watch?v=$v" -f "bestvideo[height<=900]+bestaudio[ext=m4a]/best[height<=900]" -S "codec:h264" --restrict-filenames --windows-filenames -o "/zc/put/cunt/%(title)s-%(channel)s-%(upload_date)s-%(extractor)s-%(resolution)s-%(id)s.%(ext)s"
 else
- echo RES
- echo "$res"
- yt-dlp "https://www.youtube.com/watch?v=$v" -f "bestvideo[height<=600]+bestaudio[ext=m4a]/best[height<=600]" -S "codec:h264" --restrict-filenames --windows-filenames -o "/zc/put/cunt/%(title)s-%(channel)s-%(upload_date)s-%(extractor)s-%(resolution)s-%(id)s.%(ext)s"
+    echo RES
+    echo "$res"
+    yt-dlp "https://www.youtube.com/watch?v=$v" -f "bestvideo[height<=600]+bestaudio[ext=m4a]/best[height<=600]" -S "codec:h264" --restrict-filenames --windows-filenames -o "/zc/put/cunt/%(title)s-%(channel)s-%(upload_date)s-%(extractor)s-%(resolution)s-%(id)s.%(ext)s"
 fi
 echo "</pre>"
