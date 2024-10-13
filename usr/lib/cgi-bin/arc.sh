1afdd7ef78d195a6f1e103a287ae14594e41a204
ProximaNova <jaretrudd@gmail.com>
2024-10-13 04:54:35 -0600

    Update arc.sh

--- usr/lib/cgi-bin/arc.sh
+++ usr/lib/cgi-bin/arc.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
 echo "Content-type: text/plain"
 echo
-url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
-basepath="/zc/put/cunt/warc"
-time="$(date +%s)"
-urlsafe=$(echo "$url" | sed "s/:\|\/\|?\|=\|&\|(\|)\|,\|+\|*\|%\|#/-/g")
-urllen=$(echo -n $time-$urlsafe | wc --bytes)
-if [ $urllen -gt 200 ]; then urlsafe="$(echo $urlsafe | perl -pE "s/^(.{200}).*/\1.URL2LONG/g")"; fi
+pidcount=$(ps -ef | grep -v grep | grep arc.sh | wc -l)
+if [ $pidcount -gt 5 ]; then
+    echo "Too many arc.sh PID(s) detected, exiting..."
+    exit 0
+else
+    url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
+    basepath="/zc/put/cunt/warc"
+    time="$(date +%s)"
+    urlsafe=$(echo "$url" | sed "s/:\|\/\|?\|=\|&\|(\|)\|,\|+\|*\|%\|#/-/g")
+    urllen=$(echo -n $time-$urlsafe | wc --bytes)
+    if [ $urllen -gt 200 ]; then
+        urlsafe="$(echo $urlsafe | perl -pE "s/^(.{200}).*/\1.URL2LONG/g")"
+    fi
 
-echo -n "url is: "; echo "$url" | tee $basepath/$time-$urlsafe.txt
-echo "urlsafe:$urlsafe"
-echo "time is:$time"; echo
+    echo -n "url is: "; echo "$url" | tee $basepath/$time-$urlsafe.txt
+    echo "urlsafe:$urlsafe"
+    echo "time is:$time"; echo
 
-echo "Main CID:"
+    echo "Main CID:"
 #-- wget, recursive:
 #TZ=UTC wget --no-check-certificate -pr --adjust-extension --convert-links --restrict-file-names=windows --warc-max-size=99123456 --warc-cdx -e robots=off --warc-file=$basepath/$time "$url" 1>$basepath/wget$time.txt 2>$basepath/wget$time.txt
-#-- wget, not recursive:
-cd $basepath; TZ=UTC wget --no-check-certificate -p --span-hosts --adjust-extension --convert-links --restrict-file-names=windows --warc-max-size=99123456 --warc-cdx -e robots=off --warc-file=$basepath/$time-$urlsafe "$url" 2>/dev/null #2> is saved to meta warc
+    #-- wget, not recursive:
+    cd $basepath; TZ=UTC wget --no-check-certificate -p --span-hosts --adjust-extension --convert-links --restrict-file-names=windows --warc-max-size=99123456 --warc-cdx -e robots=off --warc-file=$basepath/$time-$urlsafe "$url" 2>/dev/null #2> is saved to meta warc
+    #-- page requisites (-p) downloaded like 150 MB of videos
+    #-- from https://wiki.froth.zone/wiki/Prelinger_Archives
+    #-- so make an option to have -p off
 #-- grab-site:
-#...
+#-- ...
 #-- lynx:
-#...
+#-- ...
 
-curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe-00000.warc.gz&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
-curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe.txt&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
-curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe-meta.warc.gz&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
-curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe.cdx&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
-curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe.ipfs.txt&rawleaves=true" > $basepath/$time-$urlsafe.ipfs.set.txt; cat $basepath/$time-$urlsafe.ipfs.set.txt; echo
+    curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe-00000.warc.gz&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
+    curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe.txt&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
+    curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe-meta.warc.gz&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
+    curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe.cdx&rawleaves=true" >> $basepath/$time-$urlsafe.ipfs.txt
+    curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=$basepath/$time-$urlsafe.ipfs.txt&rawleaves=true" > $basepath/$time-$urlsafe.ipfs.set.txt; cat $basepath/$time-$urlsafe.ipfs.set.txt; echo
 
-echo "Which CONTAINS:"
-cat $basepath/$time-$urlsafe.ipfs.txt; echo
+    echo "Which CONTAINS:"
+    cat $basepath/$time-$urlsafe.ipfs.txt; echo
 
-echo "Which are these 5 CIDs:"
-maincid=$(cat $basepath/$time-$urlsafe.ipfs.set.txt | jq .Hash | sed "s/\"//g")
-subcids=$(cat $basepath/$time-$urlsafe.ipfs.txt | jq .Hash | sed "s/\"//g" | perl -pE "s/\n/ /g")
-echo $maincid.$(echo $subcids | sed "s/ /./g"); echo
+    echo "Which are these 5 CIDs:"
+    maincid=$(cat $basepath/$time-$urlsafe.ipfs.set.txt | jq .Hash | sed "s/\"//g")
+    subcids=$(cat $basepath/$time-$urlsafe.ipfs.txt | jq .Hash | sed "s/\"//g" | perl -pE "s/\n/ /g")
+    echo $maincid.$(echo $subcids | sed "s/ /#/g"); echo
 
-echo "== Copying to HPC =="; echo
-echo $maincid $subcids | tr -d \\n | xargs -d " " sh -c 'for args do TZ=UTC wget -O/dev/null http://10.0.0.232/cgi-bin/ipfsapi/v0/dag/export?arg=$args 2>&1; done' _
+    echo "== Copying to HPC =="; echo
+    echo $maincid $subcids | tr -d \\n | xargs -d " " sh -c 'for args do TZ=UTC wget -O/dev/null http://10.0.0.232/cgi-bin/ipfsapi/v0/dag/export?arg=$args 2>&1; done' _
 
-echo "== First 90K if HTML =="; echo
-zcat $basepath/$time-$urlsafe-00000.warc.gz | grep -A999999999999 "<html" | head -c90100
+    echo "== First 90K if HTML =="; echo
+    zcat $basepath/$time-$urlsafe-00000.warc.gz | grep -A999999999999 "<html" | head -c90100
+fi
