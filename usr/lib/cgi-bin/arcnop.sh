9c575cf252343b21eceea9ed58f59bf7e3c8517e
ProximaNova <jaretrudd@gmail.com>
2024-10-14 00:05:53 -0600

    Update arcnop.sh

--- usr/lib/cgi-bin/arcnop.sh
+++ usr/lib/cgi-bin/arcnop.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "Content-type: text/plain"
 echo
+#-- page requisites (-p) downloaded like 150 MB of videos
+#-- from https://wiki.froth.zone/wiki/Prelinger_Archives
+#-- so this file was made, which has -p off
 pidcount=$(ps -ef | grep -v grep | grep arcnop.sh | wc -l)
 if [[ $pidcount -gt 5 ]]; then
-    echo "Too many arcnop.sh PID(s) detected, exiting..."
+    echo "Too many arcnop.sh PID(s) detected. Try again later. PID(s):"
+    ps -ef | grep -v grep | grep arcnop.sh | grep -n $
     exit 0
 else
     url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
@@ -24,9 +28,6 @@ else
 #TZ=UTC wget --no-check-certificate -pr --adjust-extension --convert-links --restrict-file-names=windows --warc-max-size=99123456 --warc-cdx -e robots=off --warc-file=$basepath/$time "$url" 1>$basepath/wget$time.txt 2>$basepath/wget$time.txt
     #-- wget, not recursive:
     cd $basepath; TZ=UTC wget --no-check-certificate --span-hosts --adjust-extension --convert-links --restrict-file-names=windows --warc-max-size=99123456 --warc-cdx -e robots=off --warc-file=$basepath/$time-$urlsafe "$url" 2>/dev/null #2> is saved to meta warc
-    #-- page requisites (-p) downloaded like 150 MB of videos
-    #-- from https://wiki.froth.zone/wiki/Prelinger_Archives
-    #-- so make an option to have -p off
 #-- grab-site:
 #-- ...
 #-- lynx:
