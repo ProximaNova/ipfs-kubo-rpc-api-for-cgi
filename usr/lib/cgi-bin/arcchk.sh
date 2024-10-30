00de9740431c72368e63412c07b434c8a6cdf5ca
ProximaNova <jaretrudd@gmail.com>
2024-10-30 12:05:14 -0600

    Create arcchk.sh

---
+++ usr/lib/cgi-bin/arcchk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Content-type: text/plain"
+echo
+url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
+echo "$url" > /zc/put/cunt/warc/chkurl.txt
+cid=$(curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=/zc/put/cunt/warc/chkurl.txt&rawleaves=true&onlyhash=true" | jq .Hash | sed "s/\"//g")
+TZ=UTC wget --no-verbose http://10.0.0.232:8080/ipfs/$cid 2>&1 # ONLY FOR NOFETCH GATEWAYS
