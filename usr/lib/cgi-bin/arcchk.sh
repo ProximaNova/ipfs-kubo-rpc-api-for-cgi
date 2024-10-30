8d53b372096ee4d7eaebf50af4b64c3d5ec14e51
ProximaNova <jaretrudd@gmail.com>
2024-10-30 12:07:39 -0600

    Update arcchk.sh

--- usr/lib/cgi-bin/arcchk.sh
+++ usr/lib/cgi-bin/arcchk.sh
@@ -4,4 +4,4 @@ echo
 url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
 echo "$url" > /zc/put/cunt/warc/chkurl.txt
 cid=$(curl -sL "http://10.0.0.232/cgi-bin/ipfsapi/v0_edit_mpc/add?file=/zc/put/cunt/warc/chkurl.txt&rawleaves=true&onlyhash=true" | jq .Hash | sed "s/\"//g")
-TZ=UTC wget --no-verbose http://10.0.0.232:8080/ipfs/$cid 2>&1 # ONLY FOR NOFETCH GATEWAYS
+TZ=UTC wget --spider --no-verbose http://10.0.0.232:8080/ipfs/$cid 2>&1 # ONLY FOR NOFETCH GATEWAYS
