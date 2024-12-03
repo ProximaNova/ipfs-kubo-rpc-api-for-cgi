c48f77a610e325dd07f39a2f47ad6660645a22c8
ProximaNova <jaretrudd@gmail.com>
2024-12-03 03:17:11 -0700

    Update tail.sh

--- usr/lib/cgi-bin/tail.sh
+++ usr/lib/cgi-bin/tail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Content-type: text/plain"
 echo
-url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
-curl -sL "$url" | tail -c 160 | xxd
+url="$(echo -n "$REQUEST_URI" | sed "s/^\/cgi-bin\/tail.sh?url=//g")"
+curl -sLk "$url" | tail -c 160 | xxd
