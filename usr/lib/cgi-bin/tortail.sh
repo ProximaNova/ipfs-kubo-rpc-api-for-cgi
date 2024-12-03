b51591b94f5868bbfcacdde83ab1a196ad2e9046
ProximaNova <jaretrudd@gmail.com>
2024-12-03 03:20:42 -0700

    Update tortail.sh

--- usr/lib/cgi-bin/tortail.sh
+++ usr/lib/cgi-bin/tortail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Content-type: text/plain"
 echo
-url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
+url="$(echo -n "$REQUEST_URI" | sed "s/^\/cgi-bin\/tortail.sh?url=//g")"
 torsocks wget -qO- "$url" | tail -c 160 | xxd
