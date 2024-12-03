9ebd9a7645addfa42eebf894158444fa52461537
ProximaNova <jaretrudd@gmail.com>
2024-12-03 03:12:35 -0700

    Update json.sh

--- usr/lib/cgi-bin/json.sh
+++ usr/lib/cgi-bin/json.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Content-type: application/json"
 echo
-url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
+url="$(echo -n "$REQUEST_URI" | sed "s/^\/cgi-bin\/json.sh?url=//g")"
 curl -sL "$url" | jq
