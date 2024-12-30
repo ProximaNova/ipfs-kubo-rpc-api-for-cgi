10ca45402968ad587d85a9e1af1312da4b328d9b
ProximaNova <jaretrudd@gmail.com>
2024-12-29 19:32:54 -0700

    Update arcchk.sh

--- usr/lib/cgi-bin/arcchk.sh
+++ usr/lib/cgi-bin/arcchk.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Content-type: text/plain"
 echo
-url="$(echo -n "$REQUEST_URI" | sed "s/.*?url=//g")"
+localip="10.0.0.232"
+url="$(echo -n "$REQUEST_URI" | sed "s/^\/cgi-bin\/arcchk?url=//g")"
 echo "$url" > /zc/put/cunt/warc/chkurl.txt
-cid=$(curl -sL "http://10.0.0.231/cgi-bin/ipfsapi/v0_edit_mpc/add?file=/zc/put/cunt/warc/chkurl.txt&rawleaves=true&onlyhash=true" | jq .Hash | sed "s/\"//g")
-TZ=UTC wget --spider --no-verbose http://10.0.0.231:8080/ipfs/$cid 2>&1
+cid=$(curl -sLk "https://$localip/cgi-bin/ipfsapi/v0_edit_mpc/add?file=/zc/put/cunt/warc/chkurl.txt&rawleaves=true&onlyhash=true" | jq .Hash | sed "s/\"//g")
+TZ=UTC wget --spider --no-verbose --no-check-certificate https://$localip/ipfs/$cid 2>&1
 echo
 echo "Note: currently only works on NoFetch gateways."
