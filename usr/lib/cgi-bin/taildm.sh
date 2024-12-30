f1d357cf76dc7d0448f7a0d47a4136fee673d4c0
ProximaNova <jaretrudd@gmail.com>
2024-12-29 20:51:26 -0700

    Update and rename taildm.sh to taildm

--- usr/lib/cgi-bin/taildm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Content-type: text/plain"
-echo
-localip="10.0.0.231"
-id="$(echo -n "$REQUEST_URI" | sed "s/^\/cgi-bin\/taildm.sh?id=//g")"
-curl -sLk "https://$localip/cgi-bin/tail.sh?url=https://$localip/cgi-bin/ipfsapi/v0_edit_mpc/dag/export?arg=$id"
